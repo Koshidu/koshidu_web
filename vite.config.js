d9cf931a3a36bb8df0d5b4670c8907966e5d9af3
DESKTOP-POH83CC\User <koshidu.pietersz@finzlab.com>
2025-11-07 21:38:35 +0530

    Configure deployment paths

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/koshidu_website/', // 👈 Add this line
+  base: '/koshidu_web/', // 👈 Add this line
 })
 
 
